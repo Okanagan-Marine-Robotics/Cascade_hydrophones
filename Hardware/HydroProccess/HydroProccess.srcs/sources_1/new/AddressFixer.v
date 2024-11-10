aa778a94a841563d0dc433fa4622d363146420c9
JamesWilliamson999 <157648828+JamesWilliamson999@users.noreply.github.com>
2024-11-09 23:46:14 -0800

    removed files

--- Hardware/HydroProccess/HydroProccess.srcs/sources_1/new/AddressFixer.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-module AddressFixer(
-    input signed [15:0] counter,
-    output [13:0] address
-    );
-    
-assign address = (counter + 2000 < 4000&&counter + 2000 > 0)?counter + 2000:0;
-    
-endmodule
