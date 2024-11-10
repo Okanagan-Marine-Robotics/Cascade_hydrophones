6a0815606248f937afc09d8e28977cc4fefff35a
JamesWilliamson999 <157648828+JamesWilliamson999@users.noreply.github.com>
2024-11-09 23:53:26 -0800

    Revert "Merge"
    This reverts commit a0302fd6ac6f03949c2015b41fc0b60528c3bd95, reversing
    changes made to 6f67856a99b977c468f87479c875a7b8f2f975fd.

---
+++ WorkspaceOMDHydrophones/Hardware/HydroProccess/HydroProccess.gen/sources_1/bd/MicroBlaze/hdl/MicroBlaze_wrapper.v
@@ -0,0 +1,37 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2023.2 (win64) Build 4029153 Fri Oct 13 20:14:34 MDT 2023
+//Date        : Thu Nov  7 18:02:12 2024
+//Host        : DESKTOP-6IC8QHR running 64-bit major release  (build 9200)
+//Command     : generate_target MicroBlaze_wrapper.bd
+//Design      : MicroBlaze_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module MicroBlaze_wrapper
+   (clk,
+    led1,
+    reset,
+    rx,
+    tx);
+  input clk;
+  output [0:0]led1;
+  input reset;
+  input rx;
+  output tx;
+
+  wire clk;
+  wire [0:0]led1;
+  wire reset;
+  wire rx;
+  wire tx;
+
+  MicroBlaze MicroBlaze_i
+       (.clk(clk),
+        .led1(led1),
+        .reset(reset),
+        .rx(rx),
+        .tx(tx));
+endmodule
--- WorkspaceOMDHydrophones/Software/MicroBlaze_wrapper/microblaze_0/standalone_microblaze_0/bsp/microblaze_0/lib/xil_mem.d
+++
@@ -1,3 +0,0 @@
-../../../lib/xil_mem.o: xil_mem.c xil_types.h xil_mem.h
-xil_types.h:
-xil_mem.h:
